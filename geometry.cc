539fdb96c166a0d4694563d75e246f72749151c8
Gor Stepanyan <gor.stepanyan027@gmail.com>
2020-11-22 17:25:44 +0400

    Rectangle class created, some changes in previous version

--- geometry.cc
+++ geometry.cc
@@ -1,124 +1,209 @@
 #include <iostream>
 #include <typeinfo>
+#include <cassert>
 #include "geometry.h"
 
 using namespace std;
 
 Vector::Vector(int a, int b) {
-  my_x = a;
-  my_y = b;
+    my_x = a;
+    my_y = b;
 }
 
 //konstruktor kopiujący
 Vector::Vector(const Position &pos) {
-  my_x = pos.x();
-  my_y = pos.y();
-  cout<<"copying constructor vector\n";
+    my_x = pos.x();
+    my_y = pos.y();
+    cout << "copying constructor vector\n";
 }
+
 //konstruktor przenoszący
 Vector::Vector(Position &&pos) {
-  my_x = move(pos.x());
-  my_y = move(pos.y());
-  cout<<"moving constructor vector\n";
+    my_x = pos.x();
+    my_y = pos.y();
+    cout << "moving constructor vector\n";
 }
 
-bool Vector::operator== (const Vector &vec) {
-  return (my_x == vec.x() && my_y == vec.y());
+bool Vector::operator==(const Vector &other) const {
+    return (my_x == other.x() && my_y == other.y());
 }
 
-Vector& Vector::operator+= (const Vector &vec) {
-  my_x += vec.x();
-  my_y += vec.y();
-  return *this;
+Vector &Vector::operator+=(const Vector &vec) {
+    my_x += vec.x();
+    my_y += vec.y();
+
+    return *this;
 }
 
-Vector Vector::reflection() {
-  return Vector(my_y, my_x);
+Vector Vector::reflection() const {
+    return Vector(my_y, my_x);
 }
 
 int Vector::x() const {
-  return my_x;
+    return my_x;
 }
+
 int Vector::y() const {
-  return my_y;
+    return my_y;
 }
 
 void Vector::wypisz() {
-  cout<<my_x<<" "<<my_y<<endl;
+    cout << my_x << " " << my_y << endl;
 }
 
 Position::Position(int a, int b) {
-  //cout<<"tworze Position "<<a<<" "<<b<<endl;
-  my_x = a;
-  my_y = b;
+    //cout<<"tworze Position "<<a<<" "<<b<<endl;
+    my_x = a;
+    my_y = b;
 }
-
 
 //konstruktor kopiujący
 Position::Position(const Vector &vec) {
-  my_x = vec.x();
-  my_y = vec.y();
-  cout<<"copying constuctor position\n";
+    my_x = vec.x();
+    my_y = vec.y();
+    cout << "copying constuctor position\n";
 }
+
 //konstruktor przenoszący
 Position::Position(Vector &&vec) {
-  my_x = move(vec.x());
-  my_y = move(vec.y());
-  cout<<"moving constuctor position\n";
+    my_x = vec.x();
+    my_y = vec.y();
+    cout << "moving constuctor position\n";
 }
 
-bool Position::operator== (const Position &pos) {
-  return (my_x == pos.x() && my_y == pos.y());
+bool Position::operator==(const Position &other) const {
+    return (my_x == other.x() && my_y == other.y());
 }
 
-Position& Position::operator+= (const Vector &vec) {
-  my_x += vec.x();
-  my_y += vec.y();
-  return *this;
+Position &Position::operator+=(const Vector &vec) {
+    my_x += vec.x();
+    my_y += vec.y();
+    return *this;
 }
 
-Position Position::reflection() {
-  return Position(my_y, my_x);
+Position Position::reflection() const {
+    return Position(my_y, my_x);
 }
 
-int Position::x() const{
-  return my_x;
-}
-int Position::y() const{
-  return my_y;
+int Position::x() const {
+    return my_x;
 }
 
-const Position& Position::origin() {
-  static const Position ans(0, 0);
-  return ans;
+int Position::y() const {
+    return my_y;
+}
+
+const Position &Position::origin() {
+    static const Position ans(0, 0);
+    return ans;
 }
 
 void Position::wypisz() {
-  cout<<my_x<<" "<<my_y<<endl;
+    cout << my_x << " " << my_y << endl;
 }
 
-
-Position operator+ (const Position &pos, const Vector &vec) {
-  return (Position)(pos) += vec;
-}
-Position operator+ (const Vector &vec, const Position &pos) {
-  return (Position)(pos) += vec;
-}
-Vector operator+ (const Vector &vec1, const Vector &vec2) {
-  return (Vector)(vec1) += vec2;
+Position operator+(const Position &pos, const Vector &vec) {
+    return (Position) (pos) += vec;
 }
 
+Position operator+(const Vector &vec, const Position &pos) {
+    return (Position) (pos) += vec;
+}
 
+Vector operator+(const Vector &vec1, const Vector &vec2) {
+    return (Vector) (vec1) += vec2;
+}
+
+Rectangle Vector::operator+(const Rectangle &rhs) const {
+    Rectangle result(rhs);
+    result += *this;
+
+    return result;
+}
+
+Rectangle::Rectangle(int width, int height) : recPosition(0, 0) {
+    assert(width > 0 && height > 0);
+    recWidth = width;
+    recHeight = height;
+}
+
+Rectangle::Rectangle(int width, int height, Position pos) : recPosition(pos) {
+    assert(width > 0 && height > 0);
+    recWidth = width;
+    recHeight = height;
+}
+
+bool Rectangle::operator==(const Rectangle &rhs) {
+    return recWidth == rhs.recWidth && recHeight == rhs.recHeight && recPosition == rhs.recPosition;
+}
+
+int Rectangle::width() const {
+    return recWidth;
+}
+
+int Rectangle::height() const {
+    return recHeight;
+}
+
+Position Rectangle::pos() const {
+    return recPosition;
+}
+
+Rectangle Rectangle::reflection() const {
+    return Rectangle(recHeight, recWidth, recPosition.reflection());
+}
+
+int Rectangle::area() const {
+    return recWidth * recHeight;
+}
+
+Rectangle &Rectangle::operator+=(const Vector &rhs) {
+    recPosition += rhs;
+
+    return *this;
+}
+
+Rectangle Rectangle::operator+(const Vector &rhs) const {
+    Rectangle result = *this;
+    result += rhs;
+
+    return result;
+}
+
+void printRectangle(const Rectangle &rec) {
+    cout << "starting printing rectangle..." << endl;
+    rec.pos().wypisz();
+    cout << "width: " << rec.width() << " " << "height: " << rec.height() << endl;
+    cout << "ending printing rectangle" << endl << endl;
+}
 
 int main() {
-  /*
-  Position p1(1, 2);
-  Vector v1(p1);
-  Vector v2(2, 3);
-  Position p2(v2);
-  p2 = Position(v1);
-  v1 = Vector(p2);
-  */
-  Position p = Position(Vector(Position(1,2)));
-  p.wypisz();
+
+    Position p1(1, 2);
+    Vector v1(p1);
+    Vector v2(2, 3);
+    Position p2(v2);
+    p2 = Position(v1);
+    v1 = Vector(p2);
+
+//    Position p = Position(Vector(Position(1, 2)));
+//    p.wypisz();
+
+    cout << endl << "============ RECTANGLES ============" << endl << endl;
+
+    Rectangle r1(1, 1, p1);
+
+    printRectangle(r1);
+
+    Rectangle r2 = r1.reflection();
+
+    printRectangle(r2);
+
+    Rectangle r3 = r1 + v1;
+
+    printRectangle(r3);
+
+    Rectangle r4 = v1 + r1;
+
+    printRectangle(r4);
 }
--- geometry.h
+++ geometry.h
@@ -1,61 +1,100 @@
 #ifndef GEOMETRY_H
 
-  class Position;
-  
-  class Vector {
+class Position;
+
+class Rectangle;
+
+class Vector {
     int my_x;
     int my_y;
 
-    public:
-      Vector() = delete;
-      Vector(int a, int b);
-      
-      //konstruktor kopiujący
-      Vector(const Position &pos);
-      //konstruktor przenoszący
-      Vector(Position &&pos);
-   
-      bool operator== (const Vector &vec);
-      
-      Vector& operator+= (const Vector &vec);
-      
-      Vector reflection();
-      
-      int x() const;
-      int y() const;
-      
-      void wypisz();
-  };
-  
-  class Position {
+public:
+    Vector() = delete;
+
+    Vector(int a, int b);
+
+    //konstruktor kopiujący
+    explicit Vector(const Position &pos);
+
+    //konstruktor przenoszący
+    explicit Vector(Position &&pos);
+
+    bool operator==(const Vector &other) const;
+
+    Vector &operator+=(const Vector &vec);
+
+    Vector reflection() const;
+
+    int x() const;
+
+    int y() const;
+
+    Rectangle operator+(const Rectangle &rhs) const;
+
+    void wypisz();
+};
+
+class Position {
     int my_x;
     int my_y;
-    
-    public:
-      Position() = delete;
-      Position(int a, int b);
-      
-      //konstruktor kopiujący
-      Position(const Vector &vec);
-      //konstruktor przenoszący
-      Position(Vector &&vec);
-      
-      bool operator== (const Position &pos);
-      
-      Position& operator+= (const Vector &vec);
-      
-      Position reflection();
-      
-      int x() const;
-      int y() const;
-      
-      static const Position& origin();
-      
-      void wypisz();
-  };
-  
-  Position operator+ (const Position &pos, const Vector &vec);
-  Position operator+ (const Vector &vec, const Position &pos);
-  Vector operator+ (const Vector &vec1, const Vector &vec2);
-  
+
+public:
+    Position() = delete;
+
+    Position(int a, int b);
+
+    //konstruktor kopiujący
+    explicit Position(const Vector &vec);
+
+    //konstruktor przenoszący
+    explicit Position(Vector &&vec);
+
+    bool operator==(const Position &other) const;
+
+    Position &operator+=(const Vector &vec);
+
+    Position reflection() const;
+
+    int x() const;
+
+    int y() const;
+
+    static const Position &origin();
+
+    void wypisz();
+};
+
+class Rectangle {
+    Position recPosition;
+    int recWidth;
+    int recHeight;
+
+public:
+    Rectangle(int width, int height);
+
+    Rectangle(int width, int height, Position pos);
+
+    bool operator==(const Rectangle &rhs);
+
+    int width() const;
+
+    int height() const;
+
+    Position pos() const;
+
+    Rectangle reflection() const;
+
+    int area() const;
+
+    Rectangle &operator+=(const Vector &rhs);
+
+    Rectangle operator+(const Vector &rhs) const;
+};
+
+Position operator+(const Position &pos, const Vector &vec);
+
+Position operator+(const Vector &vec, const Position &pos);
+
+Vector operator+(const Vector &vec1, const Vector &vec2);
+
 #endif /* GEOMETRY_H */
